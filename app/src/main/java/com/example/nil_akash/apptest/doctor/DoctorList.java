f8dcf0d38d9703e9b8042d8d9657e87d2e681cd6
Joy Reloaded <j0yr3l0ad3d@gmail.com>
2017-04-08 05:54:11 +0600

    doctor add

--- app/src/main/java/com/example/nil_akash/apptest/doctor/DoctorList.java
+++ app/src/main/java/com/example/nil_akash/apptest/doctor/DoctorList.java
@@ -1,15 +1,25 @@
 package com.example.nil_akash.apptest.doctor;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.nil_akash.apptest.R;
 
 public class DoctorList extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_list);
+    }
+
+    public void addDoctor(View view) {
+        Intent intent = new Intent(getApplicationContext(),DoctorAdd.class);
+        intent.putExtra("flag","add");
+        startActivity(intent);
     }
 }
